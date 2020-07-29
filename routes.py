68a882d74ba2186641d43bafda7d899e056e25a2
Jimmy Wei <jimmy.wei@mindstronghealth.com>
2020-07-29 16:11:56 -0700

    add additional error handling and add more tests

--- routes.py
+++ routes.py
@@ -8,16 +8,27 @@ def routes(app):
 
     @app.route("/test", methods=['POST'])
     def get_every_third_letter():
-        if request.method == 'POST':
+        print(request.get_json())
+        posted_data = ""
+        input_string = ""
+        return_string = ""
+
+        if request.get_json() != None:
             posted_data = request.get_json()
+        else:
+            return jsonify({'error': 'Unable to process posted data!'}), 422 
+
+        if 'string_to_cut' in posted_data:
             input_string = posted_data['string_to_cut']
-            return_string = ""
-            
-            if not isinstance(input_string, unicode):
-                return jsonify({'error': 'Data Type Error! Please enter string'}), 422 
-            if len(input_string) < 3:
-                return jsonify({'return_string': ""}), 201
-            else:
-                for character in input_string[2::3]:
-                    return_string += character
-                return jsonify({'return_string': return_string}), 201
+        else:
+            return jsonify({'error': 'Unable to process posted data!'}), 422 
+        
+        if not isinstance(input_string, unicode):
+            return jsonify({'error': 'Data Type Error! Please enter string'}), 422 
+
+        if len(input_string) < 3:
+            return jsonify({'return_string': ""}), 201
+        else:
+            for character in input_string[2::3]:
+                return_string += character
+            return jsonify({'return_string': return_string}), 201
--- test_routes.py
+++ test_routes.py
@@ -2,10 +2,13 @@ from flask import Flask
 from routes import routes
 import json
 
+app = Flask(__name__)
+routes(app)
+client = app.test_client()
+mock_request_header = {"Content-Type": "application/json"}
+mock_request_data = {"string_to_cut": "iamyourlyftdriver"}
+
 def test_base_route():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
     url = "/"
 
     response = client.get(url)
@@ -13,25 +16,14 @@ def test_base_route():
     assert response.get_data() == "Home Page"
 
 def test_post_test_route_success():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
     url = "/test"
-
-    mock_request_header = {"Content-Type": "application/json"}
-    mock_request_data = {"string_to_cut": "iamyourlyftdriver"}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
     assert response.status_code == 201
     assert response.get_data().rstrip("\n") == '{"return_string":"muydv"}'
 
 def test_post_test_route_success_length_than_three():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
     url = "/test"
-
-    mock_request_header = {"Content-Type": "application/json"}
     mock_request_data = {"string_to_cut": "ia"}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
@@ -39,52 +31,55 @@ def test_post_test_route_success_length_than_three():
     assert response.get_data().rstrip("\n") == '{"return_string":""}'
 
 def test_post_test_route_success_special_character():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
     url = "/test"
-
-    mock_request_header = {"Content-Type": "application/json"}
     mock_request_data = {"string_to_cut": "@@@###$$$!!!^^^&&&"}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
     assert response.status_code == 201
     assert response.get_data().rstrip("\n") == '{"return_string":"@#$!^&"}'
 
-def test_post_test_route_failure_invalid_data_type():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
+def test_post_test_route_success_space_character():
     url = "/test"
+    mock_request_data = {"string_to_cut": "      "}
 
-    mock_request_header = {"Content-Type": "application/json"}
+    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
+    assert response.status_code == 201
+    assert response.get_data().rstrip("\n") == '{"return_string":"  "}'
+
+def test_post_test_route_failure_invalid_data_type_integer():
+    url = "/test"
     mock_request_data = {"string_to_cut": 12354}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
     assert response.status_code == 422
     assert response.get_data().rstrip("\n") == '{"error":"Data Type Error! Please enter string"}'
 
-def test_post_test_route_failure_null_input():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
+def test_post_test_route_failure_invalid_data_type_boolean():
     url = "/test"
-
-    mock_request_header = {"Content-Type": "application/json"}
-    mock_request_data = {"string_to_cut": None}
+    mock_request_data = {"string_to_cut": True}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
     assert response.status_code == 422
     assert response.get_data().rstrip("\n") == '{"error":"Data Type Error! Please enter string"}'
 
 def test_post_test_route_failure_invalid_json():
-    app = Flask(__name__)
-    routes(app)
-    client = app.test_client()
     url = "/test"
+    mock_request_data = None
 
-    mock_request_header = {"Content-Type": "application/json"}
+    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
+    assert response.status_code == 422
+    assert response.get_data().rstrip("\n") == '{"error":"Unable to process posted data!"}'
+
+def test_post_test_route_failure_empty_json():
+    url = "/test"
     mock_request_data = {}
 
     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_header)
-    assert response.status_code == 500
+    assert response.status_code == 422
+    assert response.get_data().rstrip("\n") == '{"error":"Unable to process posted data!"}'
+
+def test_post_test_route_failure_without_body():
+    url = "/test"
+
+    response = client.post(url, headers=mock_request_header)
+    assert response.status_code == 400
